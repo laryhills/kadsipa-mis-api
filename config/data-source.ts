10ef99f34dd9b2c9f17c24209441d95670de0972
laryhills <hilaryokoh@gmail.com>
2026-04-21 13:37:13 +0100

    chore: update data-source to use 'path'

--- config/data-source.ts
+++ config/data-source.ts
@@ -1,9 +1,14 @@
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
+import path from 'path';
+
 // dotenv.config();
+
 const envFile =
-  process.env.NODE_ENV === 'production' ? '.env' : '.env.development';
-dotenv.config({ path: envFile });
+  process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : '.env.development';
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
