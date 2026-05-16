0f25575d1104b7b5db4343b6547f27f596e374d8
laryhills <hilaryokoh@gmail.com>
2026-05-16 11:19:03 +0100

    refactor: remove email domain validation from InviteUserDto

--- src/users/dto/invite-user.dto.ts
+++ src/users/dto/invite-user.dto.ts
@@ -3,7 +3,7 @@ import {
   IsEmail,
   IsNotEmpty,
   IsString,
-  Matches,
+  // Matches,
   IsArray,
   IsUUID,
   IsOptional,
@@ -13,9 +13,9 @@ export class InviteUserDto {
   @IsNotEmpty({ message: 'Email is required' })
   @IsEmail()
   @Transform(({ value }: { value: string }) => value.trim())
-  @Matches(/@(kdsg|kadsipa)\.gov\.ng$/, {
-    message: 'Email domain must be @kdsg.gov.ng or @kadsipa.gov.ng',
-  })
+  // @Matches(/@(kdsg|kadsipa)\.gov\.ng$/, {
+  //   message: 'Email domain must be @kdsg.gov.ng or @kadsipa.gov.ng',
+  // })
   email: string;
 
   @IsNotEmpty({ message: 'Full name is required' })
